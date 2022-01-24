431e00dd3c1b5db4f9d4c7ffe1131bcb0e3f7e2f
Charles Pinheiro <charles_fagundes@hotmail.com>
2022-01-24 15:53:08 -0300

    feat: delete e update

--- src/main/java/com/apirest/backend/controllers/FuncionarioController.java
+++ src/main/java/com/apirest/backend/controllers/FuncionarioController.java
@@ -7,14 +7,16 @@ import com.apirest.backend.repo.FuncionarioRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -37,6 +39,19 @@ public class FuncionarioController {
     @PostMapping("/funcionario")
     @ResponseStatus(HttpStatus.CREATED)
     public Funcionario salvaFuncionario(@RequestBody Funcionario funcionario) {
+        return funcionarioRepo.save(funcionario);
+    }
+
+    @DeleteMapping("/funcionario/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletaFuncionario(@PathVariable(value="id") long id) {
+        Funcionario funcionario = funcionarioRepo.getById(id);
+        funcionarioRepo.delete(funcionario);
+    }
+
+    @PutMapping("/funcionario/{id}")
+    public Funcionario atualizaFuncionario(@PathVariable(value="id") long id, @RequestBody Funcionario funcionario) {
+        funcionario.setId(id);
         return funcionarioRepo.save(funcionario);
     }
 }
