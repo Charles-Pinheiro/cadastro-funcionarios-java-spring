dc9ab8a5624cb15a2a563fe9b189e0f9afe2d612
Charles Pinheiro <charles_fagundes@hotmail.com>
2022-01-24 15:16:20 -0300

    feat: get all

--- src/main/java/com/apirest/backend/controllers/FuncionarioController.java
+++ src/main/java/com/apirest/backend/controllers/FuncionarioController.java
@@ -1,8 +1,26 @@
 package com.apirest.backend.controllers;
 
+import java.util.List;
+
+import com.apirest.backend.models.Funcionario;
+import com.apirest.backend.repo.FuncionarioRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/api")
 public class FuncionarioController {
+
+    @Autowired
+    FuncionarioRepo funcionarioRepo;
+
+    @GetMapping("/funcionarios")
+    public List<Funcionario> listaFuncionarios() {
+        return funcionarioRepo.findAll();
+    }
+
     
 }
