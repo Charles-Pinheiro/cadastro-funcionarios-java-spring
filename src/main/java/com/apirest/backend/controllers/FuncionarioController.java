0090e7a8d4ceab0427f16c6a90fcc84129baa13b
Charles Pinheiro <charles_fagundes@hotmail.com>
2022-01-24 15:31:00 -0300

    feat: get por id e post

--- src/main/java/com/apirest/backend/controllers/FuncionarioController.java
+++ src/main/java/com/apirest/backend/controllers/FuncionarioController.java
@@ -6,9 +6,16 @@ import com.apirest.backend.models.Funcionario;
 import com.apirest.backend.repo.FuncionarioRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 @RestController
 @RequestMapping(value="/api")
@@ -22,5 +29,14 @@ public class FuncionarioController {
         return funcionarioRepo.findAll();
     }
 
-    
+    @GetMapping("/funcionario/{id}")
+    public Funcionario listaPorId(@PathVariable(value="id") long id) {
+        return funcionarioRepo.findById(id);
+    }
+
+    @PostMapping("/funcionario")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Funcionario salvaFuncionario(@RequestBody Funcionario funcionario) {
+        return funcionarioRepo.save(funcionario);
+    }
 }
--- src/main/java/com/apirest/backend/repo/FuncionarioRepo.java
+++ src/main/java/com/apirest/backend/repo/FuncionarioRepo.java
@@ -5,5 +5,6 @@ import com.apirest.backend.models.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuncionarioRepo extends JpaRepository<Funcionario, Long> {
-    
+
+    Funcionario findById(long id);
 }
